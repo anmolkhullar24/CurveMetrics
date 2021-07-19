986ae7a0ebd476ac3179d0f84c2ecc71dfe28328
anmolkhullar24 <anmolkhullar2405@gmail.com>
2021-07-19 16:20:51 +0530

    login and signup working
    login and signup working

---
+++ lib/models/Product.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  final int id;
+  final String title, description;
+  final List<String> images;
+  final List<Color> colors;
+  final double rating, price;
+  final bool isFavourite, isPopular;
+
+  Product({
+    @required this.id,
+    @required this.images,
+    @required this.colors,
+    this.rating = 0.0,
+    this.isFavourite = false,
+    this.isPopular = false,
+    @required this.title,
+    //@required this.price,
+    this.price,
+    @required this.description,
+  });
+}
+
+// Our demo Products
+
+List<Product> demoProducts = [
+  Product(
+    id: 1,
+    images: [
+      "assets/images/model1.jpeg",
+      //"assets/images/ps4_console_white_2.png",
+      //"assets/images/ps4_console_white_3.png",
+      //"assets/images/ps4_console_white_4.png",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Suit 1 ™",
+    //price: 64.99,
+    description: description,
+    rating: 4.8,
+    isFavourite: true,
+    isPopular: true,
+  ),
+  Product(
+    id: 2,
+    images: [
+      "assets/images/model2.jpeg",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Suit 2",
+    //price: 50.5,
+    description: description,
+    rating: 4.1,
+    isPopular: true,
+  ),
+  // Product(
+  //   id: 3,
+  //   images: [
+  //     "assets/images/glap.png",
+  //   ],
+  //   colors: [
+  //     Color(0xFFF6625E),
+  //     Color(0xFF836DB8),
+  //     Color(0xFFDECB9C),
+  //     Colors.white,
+  //   ],
+  //   title: "Gloves XC Omega - Polygon",
+  //   price: 36.55,
+  //   description: description,
+  //   rating: 4.1,
+  //   isFavourite: true,
+  //   isPopular: true,
+  // ),
+  // Product(
+  //   id: 4,
+  //   images: [
+  //     "assets/images/wireless headset.png",
+  //   ],
+  //   colors: [
+  //     Color(0xFFF6625E),
+  //     Color(0xFF836DB8),
+  //     Color(0xFFDECB9C),
+  //     Colors.white,
+  //   ],
+  //   title: "Logitech Head",
+  //   price: 20.20,
+  //   description: description,
+  //   rating: 4.1,
+  //   isFavourite: true,
+  // ),
+];
+
+const String description = "Suits for sale";
